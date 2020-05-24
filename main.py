52f19e5457599d10cb686905c180df10a4ebd0f0
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-05-24 16:04:25 -0300

    Lista de Meses e dias

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-# PREPARANDO VARIAVEIS
+# PREPARANDO
 
-meses = list( range(1,12));
+
+def AnoBisexto( ano ):
+    i_ano = int(ano)
+    if i_ano % 4 == 0:
+        if i_ano % 100 != 0:
+            fev = 29;
+        else:
+            fev = 28;
+    else:
+        fev = 28;  
+    return fev;
+
+
+
+
+meses = list( range(1,13));
+y = input( 'Digite um ano para saber se é bisexto: ');
+dias = [ 31, AnoBisexto(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+meses_dias = [];
+
+for i in range(1, 13):
+    for d in range(1, dias[i]):
+        meses_dias = 
 
 print( meses );
-
+print( dias )
