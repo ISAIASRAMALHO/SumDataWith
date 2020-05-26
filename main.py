67058eb7d8ddf40429864d3d391c854e4a9e90b5
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-05-26 13:07:05 -0300

    c

--- main.py
+++ main.py
@@ -20,22 +20,27 @@ y = input( 'Digite um ano para saber se é bisexto: ');
 v = int( input('Informe o valor em que a soma e dia+mes+ano coincida: ') );
 
 s, t = 0, 0;
-meses       = list( range(1, 13) );
-dias        = [ 31, fev(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-meses_dias  = [ [],[],[],[],[],[],[],[],[],[],[],[] ];
+meses       = list( range(1, 13) )
+dias        = [ 31, fev(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+meses_dias  = [ [],[],[],[],[],[],[],[],[],[],[],[] ]
 
 
 for m in meses:
     for d in range(1, dias[m-1]+1 ):
-        meses_dias[m-1].append(d);
-        
+        meses_dias[m-1].append(d)
+        print('{}/{}/{}'.format(d, m, y))
+        s = s+1
+    
+
+
+print(s)        
 
 # SOMAR CADA DATA DO CALENDARIO DO ANO SELECIONADO.
 
-for m in meses:
-    for d in range(1, meses_dias[m-1][m-1]):
-        s = d + m + int(y);
-        if s == v:
-            t=t+1;
+#for m in meses:
+#    for d in range(1, meses_dias[m] ):
+#        s = d + m + int(y);
+#        if s == v:
+#            t=t+1;
 
-print( t);
+
