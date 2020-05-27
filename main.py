3745fc1574fb1694618257b14fe81bfb556bbe7e
ISAIASRAMALHO <isaiasramalhoepai@gmail.com>
2020-05-27 18:56:19 -0300

    Catch one

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def AnoBisexto( ano ):
     i_ano = int(ano)
     if i_ano % 4 == 0:
         if i_ano % 100 != 0:
-            fev = 29;
+            fev = "29";
         else:
-            fev = 28;
+            fev = "28";
     else:
-        fev = 28;  
+        fev = "28";  
     return fev;
 
 
@@ -19,28 +19,32 @@ def fev( ano ):
 y = input( 'Digite um ano para saber se é bisexto: ');
 v = int( input('Informe o valor em que a soma e dia+mes+ano coincida: ') );
 
-s, t = 0, 0;
-meses       = list( range(1, 13) )
-dias        = [ 31, fev(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-meses_dias  = [ [],[],[],[],[],[],[],[],[],[],[],[] ]
+sd, sm, sa, t = 0,  0, 0, 0;
+meses       = ['01','02','03','04','05','06','07','08','09','10','11','12']
+dias        = [ "31", fev(y), "31", "30", "31", "30", "31", "31", "30", "31", "30", "31" ]
+calend  = [ [],[],[],[],[],[],[],[],[],[],[],[] ]
 
 
+
+# REPETIR INSTRUÇÕES 12 VEZES. 
 for m in meses:
-    for d in range(1, dias[m-1]+1 ):
-        meses_dias[m-1].append(d)
-        print('{}/{}/{}'.format(d, m, y))
-        s = s+1
-    
+    sd = int( m) - 1
+    sm = int( dias[sd] )
+# REPETIR INSTRUÇÕES TOTAL DE DIAS DO MÊS DA REPETIÇÃO ATUAL
+    for d in range(1, sm+1):
+        calend[sd].append(d)
 
 
-print(s)        
+for c in calend:
+    print( "Dias desse mês: {}".format( c ) )
 
-# SOMAR CADA DATA DO CALENDARIO DO ANO SELECIONADO.
-
-#for m in meses:
-#    for d in range(1, meses_dias[m] ):
-#        s = d + m + int(y);
-#        if s == v:
-#            t=t+1;
+    # for d in 
 
 
+
+
+
+#print(s)        
+
+# SOMAR CADA ALGARISMO DE UMA DATA POR VEZ EM TODO ANO SELECIONADO.
+# EXEMPLO:  31/10/2007 = 3 + 1 + 1 +0 + 2 + 0 + 0 + 7   = 14
